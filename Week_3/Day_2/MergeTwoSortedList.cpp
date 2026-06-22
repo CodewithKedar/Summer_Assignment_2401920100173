d80fe2f01151d9dfcdb5a723dd59e5ea873bc00e
CodewithKedar <kedar.1iit@gmail.com>
2026-06-23 03:51:24 +0530

    Create MergeTwoSortedList.cpp

---
+++ Week_3/Day_2/MergeTwoSortedList.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode ans(-1);
+        ListNode* temp=&ans;
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                temp->next=list1;
+                list1=list1->next;
+            }
+            else{
+                temp->next=list2;
+                list2=list2->next;
+            }
+            temp=temp->next;
+        }
+        if(list1){
+            temp->next=list1;
+        }
+        else{
+            temp->next=list2;
+        }
+        return ans.next;
+    }
+};
