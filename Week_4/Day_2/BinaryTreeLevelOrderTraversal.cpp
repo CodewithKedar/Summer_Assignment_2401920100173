79fa1b3738a043da5985d190a6b186ce5cd01154
CodewithKedar <kedar.1iit@gmail.com>
2026-06-23 20:56:49 +0530

    Implement level order traversal for binary tree

---
+++ Week_4/Day_2/BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+max(height(root->left),height(root->right));
+    }
+    void Levelorder(TreeNode* root,int level,vector<int> &temp) {
+        if(root==NULL) return;
+        if(level==1){
+            temp.push_back(root->val);
+            return;
+        }
+        
+        Levelorder(root->left,level-1,temp);
+        Levelorder(root->right,level-1,temp);
+
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        int h=height(root);
+        for(int i=1;i<=h;i++){
+            vector<int> temp;
+            Levelorder(root,i,temp);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
